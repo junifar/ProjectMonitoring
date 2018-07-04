135580d37f118f02b5cf439fb6743514b86d03e0
junifar <junifar@gmail.com>
2018-07-04 15:14:08 +0700

    pp

--- app/src/main/java/com/prodroid/projectmonitoring/MainActivity.kt
+++ app/src/main/java/com/prodroid/projectmonitoring/MainActivity.kt
@@ -1,24 +1,82 @@
 package com.prodroid.projectmonitoring
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when(v.id){
+                R.id.btn_sign_in->signIn()
+            }
+        }
+    }
+
+    private var mGoogleApiClient : GoogleApiClient? = null
+    private val REQUEST_CODE_SIGN_IN = 1000
+
+    override fun onConnectionFailed(result: ConnectionResult) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun signIn(){
+//        Toast.makeText(this, "Sample11", Toast.LENGTH_LONG).show()
+        val intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+        startActivityForResult(intent, REQUEST_CODE_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_SIGN_IN){
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result.isSuccess){
+                Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
+            }else{
+                updateUI(null)
+            }
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null){
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btn_sign_in.setOnClickListener(this)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        this.mGoogleApiClient = GoogleApiClient.Builder(this).enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build()
+
         val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("use11r")
+        val myRef = database.getReference("users")
 
         myRef.addValueEventListener(object:ValueEventListener{
             override fun onCancelled(error: DatabaseError) {
